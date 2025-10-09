0e2936f0e67ab9a34f5b49b854d0ad027f235747
sam anderson <sjandrs1@gmail.com>
2025-10-09 17:39:39 -0400

    Add shared MSW server setup for frontend test environment

---
+++ frontend/src/test/msw-server.js
@@ -0,0 +1,18 @@
+// Central MSW test server setup (v2)
+// Ensures interceptors load before other test utilities.
+// Force-load interceptor before importing msw/node
+// eslint-disable-next-line import/no-extraneous-dependencies
+import '@mswjs/interceptors/ClientRequest';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+
+// Default no-op handlers; individual test files can call server.use(...)
+export const server = setupServer();
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }));
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+// Force-load interceptors that were failing resolution in mixed ESM/CJS
+// eslint-disable-next-line import/no-extraneous-dependencies
+try { require('@mswjs/interceptors/ClientRequest'); } catch (e) { /* ignore */ }
